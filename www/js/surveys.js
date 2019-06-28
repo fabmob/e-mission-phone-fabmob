695d593ddd0aa2d14e3eeb887b9ae76f9d058b98
Loïc Mayol <l.mayol@laposte.net>
2019-06-28 05:42:15 -0700

    Improve UI of the Surveys screen

--- www/js/surveys.js
+++ www/js/surveys.js
@@ -3,8 +3,8 @@
 angular.module('emission.main.surveys', ['nvd3', 'emission.services', 'emission.survey.launch', 'emission.plugin.kvstore', 'emission.plugin.logger'])
 
 .controller('SurveysCtrl', function ($scope, $ionicActionSheet, $ionicLoading,
-                                    CommHelper, $window, $ionicPopup,
-                                    ionicDatePicker, $ionicPlatform,
+                                    CommHelper, $rootScope,
+                                    $ionicPlatform,
                                     $translate, SurveyLaunch) {
     
     $scope.surveys = {}
@@ -21,10 +21,14 @@ angular.module('emission.main.surveys', ['nvd3', 'emission.services', 'emission.
             var surveys = result.surveys;
             $scope.completedSurveys = [];
             $scope.ongoingSurveys = [];
+            Logger.log("User has "+ result.survey_count + "surveys, surveys are:" + JSON.stringify(result.surveys));
             surveys.forEach(survey => {
                 if (survey.completed == "N" && survey.active == "Y") {
+                    survey.received = moment(survey.received).format('LL');
                     $scope.ongoingSurveys.push(survey);
                 } else if (survey.completed != "N" && survey.active == "Y") {
+                    survey.completed = moment(survey.completed).format('LL');
+                    survey.received = moment(survey.received).format('LL');
                     $scope.completedSurveys.push(survey);
                 } else {
                     console.log(survey)
@@ -41,6 +45,10 @@ angular.module('emission.main.surveys', ['nvd3', 'emission.services', 'emission.
     $scope.launchSurvey = function (url) {
         console.log(url)
         SurveyLaunch.startSurvey(url, "");
+        $rootScope.$on('$cordovaInAppBrowser:exit', function(e, event) {
+            console.log("exiting, event = "+JSON.stringify(event));
+            getSurveys();
+        });
     }
 
     $ionicPlatform.ready(function () {
